043ca489d81d341694b8493b3ad2c064da4879d0
Laima2019 <55127104+Laima2019@users.noreply.github.com>
2019-10-15 16:13:57 +0300

    projektasPi

---
+++ app/function.php
@@ -0,0 +1,20 @@
+<?php
+​
+$kates = rand(1,3);
+$sunys = rand(1, 3);
+$katasuniai = 0;
+​
+for ($k=0; $k<$kates; $k++){
+    for ($s=0; $s<$sunys; $s++){
+        if (rand(0, 1)){
+            $katasuniai++; 
+            break;
+        }
+    }
+}
+​
+$h1 = 'Katasuniu iseiga';
+$h2 = "Dalyvano sunu: $sunys, kaciu: $kates";
+$h3 = "Gavosi katasuniu $katasuniai";
+​
+?>
